1441e1c2dffcda4dfbe6d59a0544b84580b895d9
raczak <kornel.raczak@nokia.com>
2019-04-29 22:25:21 +0200

    Added implementation of UnionQuickFind, QuickUnion, QuickUnionWeighted and QuickUnionPathCompression. Included also unit tests for first two of them.

---
+++ test_union.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Module responsible for unit test coverage of union module
+"""
+from unittest import TestCase
+
+from algorithms.union import QuickUnionPathCompression, UnionQuickFind, UnionQuickUnion, QuickUnionWeighted
+
+
+class TestUnionQuickFind(TestCase):
+    def setUp(self):
+        self.u = UnionQuickFind(5)
+
+    def test_union_whenPNotConnectedWithQ_thenJoinsThem(self):
+        self.u.is_connected = lambda x, y: False
+        self.u.union(0, 1)
+        self.assertEqual(self.u.union_array, [0, 0, 2, 3, 4])
+
+    def test_union_whenPConnectedWithQ_thenPass(self):
+        self.u.is_connected = lambda x, y: True
+        self.u.union(0, 1)
+        self.assertEqual(self.u.union_array, [0, 1, 2, 3, 4])
+
+    def test_is_connected_whenTrue_thenReturnTrue(self):
+        self.u.union_array = [0, 0, 2, 3, 4]
+        result = self.u.is_connected(0, 1)
+        self.assertTrue(result)
+
+    def test_is_connected_whenFalse_thenReturnFalse(self):
+        self.u.union_array = [0, 1, 2, 3, 4]
+        result = self.u.is_connected(0, 1)
+        self.assertFalse(result)
+
+
+class TestUnionQuickUnion(TestCase):
+    def setUp(self):
+        self.u = UnionQuickUnion(5)
+
+    def test_union_whenNotConnected_thenJoinsQRootToPRoot(self):
+        self.u.union_array[1] = 2
+        self.u.union(0, 1)
+        self.assertEqual(self.u.union_array, [2, 2, 2, 3, 4])
+
+    def test_union_whenConnected_thenPass(self):
+        self.u.union(0, 1)
+        self.assertEqual(self.u.union_array, [1, 1, 2, 3, 4])
+
+    def test_is_connected_whenConnected_thenReturnTrue(self):
+        self.u.union_array = [1, 1, 2, 3, 4]
+        result = self.u.is_connected(0, 1)
+        self.assertTrue(result)
+
+    def test_is_connected_whenNotConnected_thenReturnFalse(self):
+        self.u.union_array = [0, 1, 2, 3, 4]
+        result = self.u.is_connected(0, 1)
+        self.assertFalse(result)
+
+    def test_root_whenIRoot_thenReturnI(self):
+        self.assertEqual(self.u.root(2), 2)
+
+    def test_root_whenJRoot_thenReturnJ(self):
+        self.u.union_array = [0, 3, 2, 0, 4]
+        j, i = 0, 1
+        self.assertEqual(self.u.root(i), j)
+
+
+class TestQuickUnionWeighted(TestCase):
+    def setUp(self):
+        self.u = QuickUnionWeighted(5)
+
+    def test_union(self):
+        pass
+
+
+class TestQuickUnionPathCompression(TestCase):
+    def setUp(self):
+        self.u = QuickUnionPathCompression(5)
+
+    def test_union(self):
+        pass
---
+++ union.py
@@ -0,0 +1,94 @@
+import time
+
+import numpy as np
+
+
+class UnionQuickFind:
+    def __init__(self, n):
+        self.union_array = list(range(n))
+
+    def union(self, p, q):
+        if not self.is_connected(p, q):
+            tmp = self.union_array[q]
+            for i, value in enumerate(self.union_array):
+                if value == tmp:
+                    self.union_array[i] = self.union_array[p]
+
+    def is_connected(self, p, q):
+        return self.union_array[p] == self.union_array[q]
+
+    def is_one_union(self):
+        return len(set(self.union_array)) == 1
+
+    def __repr__(self):
+        return 'UnionQuickFind'
+
+
+class UnionQuickUnion:
+    def __init__(self, n):
+        self.n = n
+        self.union_array = list(range(n))
+
+    def root(self, i):
+        while i != self.union_array[i]:
+            i = self.union_array[i]
+        return i
+
+    def is_connected(self, i, j):
+        return self.root(i) == self.root(j)
+
+    def union(self, p, q):
+        self.union_array[self.root(p)] = self.root(q)
+
+    def is_one_union(self):
+        tmp_root = self.root(self.union_array[0])
+        return all([self.root(i) == tmp_root for i in self.union_array[1:]])
+
+    def __repr__(self):
+        return 'UnionQuickUnion'
+
+
+class QuickUnionWeighted(UnionQuickUnion):
+    def __init__(self, n):
+        super().__init__(n)
+        self.size_array = [1 for _ in range(n)]
+
+    def union(self, p, q):
+        p_root = self.root(p)
+        q_root = self.root(q)
+        if self.size_array[p_root] < self.size_array[q_root]:
+            self.union_array[p_root] = q_root
+            self.size_array[q_root] += self.size_array[p_root]
+        else:
+            self.union_array[q_root] = p_root
+            self.size_array[p_root] += self.size_array[q_root]
+
+    def __repr__(self):
+        return 'QuickUnionWeighted'
+
+
+class QuickUnionPathCompression(QuickUnionWeighted):
+    def __init__(self, n):
+        super().__init__(n)
+
+    def root(self, i):
+        while i != self.union_array[i]:
+            self.union_array[i] = self.union_array[self.union_array[i]]
+            i = self.union_array[i]
+        return i
+
+    def __repr__(self):
+        return 'QuickUnionPathCompression'
+
+
+n = 3 * 10**3
+unions = [UnionQuickFind, UnionQuickUnion, QuickUnionWeighted, QuickUnionPathCompression]
+vertices = [np.random.randint(0, n, 2) for i in range(n)]
+for union in unions:
+    u = union(n)
+    s = time.time()
+    for pair in vertices:
+        u.union(*pair)
+    e = time.time()
+    print(f'{u} took {e - s} seconds')
+
