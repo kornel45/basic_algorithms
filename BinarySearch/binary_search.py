58938d73093da9d3bf48f6ed3d0c361ff3c953a0
kornel43 <kornel43@gmail.com>
2019-09-12 23:35:52 +0200

    Delete not needed counting variable

--- BinarySearch/binary_search.py
+++ BinarySearch/binary_search.py
@@ -1,19 +1,16 @@
 def binary_search(value, lst):
 	"""Binary search of value in sorted list"""
-	how_many = 0
 	low = 0
 	high = len(lst)
 	while low < high:
-		how_many += 1
 		index = int((low + high)/2)
-		if lst[index] < value:
-			low = index
+		if lst[index] == value:
+			return index
 		elif lst[index] > value:
 			high = index
 		else:
-			return index, how_many
-	return -1, how_many
+			low = index
+	return None 
 
-x = binary_search(10, list(range(1500)))
-print(x)
-
+result = binary_search(10, list(range(1500)))
+print(result)
